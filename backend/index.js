8afcf8f6854198c67224d758bffc4fa8ca9ae5df
Jhan Bayer <jhanbayer@gmail.com>
2022-06-22 15:13:32 -0400

    feat: install dependencys

---
+++ backend/index.js
@@ -0,0 +1,14 @@
+import express from "express";
+import dotenv from "dotenv";
+import conectarDB from "./config/db.js";
+
+const app = express();
+
+dotenv.config();
+
+conectarDB();
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+  console.log(`servidor corriendo en el puerto ${PORT}`);
+});
