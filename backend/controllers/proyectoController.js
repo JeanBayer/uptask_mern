bf0c0ca9889e56afa50c2eee4343856c117728d7
Jhan Bayer <jhanbayer@gmail.com>
2022-06-24 16:55:41 -0400

    feat: add bolierplate for Projects

---
+++ backend/controllers/proyectoController.js
@@ -0,0 +1,26 @@
+const obtenerProyectos = (req, res) => {};
+
+const nuevoProyecto = (req, res) => {};
+
+const obtenerProyecto = (req, res) => {};
+
+const editarProyecto = (req, res) => {};
+
+const eliminarProyecto = (req, res) => {};
+
+const agregarColaborador = (req, res) => {};
+
+const eliminarColaborador = (req, res) => {};
+
+const obtenerTareas = (req, res) => {};
+
+export {
+  obtenerProyectos,
+  nuevoProyecto,
+  obtenerProyecto,
+  editarProyecto,
+  eliminarProyecto,
+  agregarColaborador,
+  eliminarColaborador,
+  obtenerTareas,
+};
--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import conectarDB from "./config/db.js";
 import usuarioRoutes from "./routes/usuarioRoutes.js";
+import proyectoRoutes from "./routes/proyectoRoutes.js";
 
 const app = express();
 app.use(express.json());
@@ -11,6 +12,7 @@ dotenv.config();
 conectarDB();
 
 app.use("/api/usuarios", usuarioRoutes);
+app.use("/api/proyectos", proyectoRoutes);
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
