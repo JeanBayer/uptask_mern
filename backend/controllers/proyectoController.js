b6e08e09f6af889f985545e51071896af8c4ab33
Jhan Bayer <jhanbayer@gmail.com>
2022-06-25 16:17:12 -0400

    feat: create put a project api

--- backend/controllers/proyectoController.js
+++ backend/controllers/proyectoController.js
@@ -19,7 +19,6 @@ const nuevoProyecto = async (req, res) => {
 };
 
 const obtenerProyecto = async (req, res) => {
-  console.log(req.usuario._id);
   const proyecto = await Proyecto.findById(req.params.id);
   if (!proyecto) {
     const error = new Error("Proyecto no encontrado");
@@ -33,7 +32,30 @@ const obtenerProyecto = async (req, res) => {
   res.send(proyecto);
 };
 
-const editarProyecto = (req, res) => {};
+const editarProyecto = async (req, res) => {
+  const proyecto = await Proyecto.findById(req.params.id);
+  if (!proyecto) {
+    const error = new Error("Proyecto no encontrado");
+    return res.status(404).send({ msg: error.message });
+  }
+
+  if (proyecto.creador.toString() !== req.usuario._id.toString()) {
+    const error = new Error("No tienes permisos para ver este proyecto");
+    return res.status(401).send({ msg: error.message });
+  }
+
+  proyecto.nombre = req.body.nombre || proyecto.nombre;
+  proyecto.descripcion = req.body.descripcion || proyecto.descripcion;
+  proyecto.fechaEntrega = req.body.fechaEntrega || proyecto.fechaEntrega;
+  proyecto.cliente = req.body.cliente || proyecto.cliente;
+
+  try {
+    const proyectoActualizado = await proyecto.save();
+    res.send(proyectoActualizado);
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 const eliminarProyecto = (req, res) => {};
 
