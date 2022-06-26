ec61f2cae2725b38c4dac262c8badab47d673cd3
Jhan Bayer <jhanbayer@gmail.com>
2022-06-26 17:34:34 -0400

    feat: add delete a task api

--- backend/controllers/tareaController.js
+++ backend/controllers/tareaController.js
@@ -56,7 +56,7 @@ const actualizarTarea = async (req, res) => {
   tarea.descripcion = req.body.descripcion || tarea.descripcion;
   tarea.fechaEntrega = req.body.fechaEntrega || tarea.fechaEntrega;
   tarea.prioridad = req.body.prioridad || tarea.prioridad;
-  
+
   try {
     const tareaActualizada = await tarea.save();
     res.json(tareaActualizada);
@@ -65,7 +65,28 @@ const actualizarTarea = async (req, res) => {
   }
 };
 
-const eliminarTarea = async (req, res) => {};
+const eliminarTarea = async (req, res) => {
+  const { id } = req.params;
+  const tarea = await Tarea.findById(id).populate("proyecto");
+  if (!tarea) {
+    const error = new Error("La tarea no existe");
+    return res.status(404).json({ msg: error.message });
+  }
+
+  if (tarea.proyecto.creador.toString() !== req.usuario._id.toString()) {
+    const error = new Error("No tienes permiso para ver esta tarea");
+    return res.status(403).json({ msg: error.message });
+  }
+
+  try {
+    await tarea.deleteOne();
+    res.json({ msg: "Tarea eliminada" });
+  } catch (error) {
+    console.log(error);
+  }
+
+};
+
 const cambiarEstado = async (req, res) => {};
 
 export {
