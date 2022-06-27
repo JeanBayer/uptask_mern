900b9c9a6308c38d4a070088904b005940ce038c
Jhan Bayer <jhanbayer@gmail.com>
2022-06-27 19:21:40 -0400

    feat: add confirm account logic

--- frontend/src/paginas/ConfirmarCuenta.jsx
+++ frontend/src/paginas/ConfirmarCuenta.jsx
@@ -1,12 +1,54 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import { useParams, Link } from "react-router-dom";
+import axios from "axios";
+import Alerta from "../components/Alerta";
 
 const ConfirmarCuenta = () => {
+  const [alerta, setAlerta] = useState({});
+  const [cuentaConfirmada, setCuentaConfirmada] = useState(false);
+  const params = useParams();
+  const { id } = params;
+
+  useEffect(() => {
+    const confirmarCuenta = async () => {
+      setAlerta({});
+      try {
+        const url = `${
+          import.meta.env.VITE_BACKEND_URL
+        }/api/usuarios/confirmar/${id}`;
+
+        const { data } = await axios(url);
+        setAlerta({ msg: data.msg, error: false });
+        setCuentaConfirmada(true);
+      } catch (error) {
+        setAlerta({ msg: error.response.data.msg, error: true });
+        setCuentaConfirmada(false);
+      }
+    };
+
+    return () => {
+      confirmarCuenta();
+    };
+  }, []);
+
+  const { msg } = alerta;
   return (
     <>
       <h1 className="text-sky-600 font-black text-6xl capitalize">
         Confirma tu cuenta y empieza a crear tus{" "}
         <span className="text-slate-700">proyectos</span>
       </h1>
+      <div className="mt-20 md:mt-10 shadow-lg px-5 py-10 rounded-xl bg-white">
+        {msg && <Alerta alerta={alerta} />}
+        {cuentaConfirmada && (
+          <Link
+            to="/"
+            className="block text-center my-5 text-slate-500 uppercase text-sm"
+          >
+            Inicia sesión
+          </Link>
+        )}
+      </div>
     </>
   );
 };
