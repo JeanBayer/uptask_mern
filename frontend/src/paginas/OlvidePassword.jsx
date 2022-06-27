9976b475c69cfd6ac39033af0e25e641e8bdfc00
Jhan Bayer <jhanbayer@gmail.com>
2022-06-26 20:53:52 -0400

    feat: add UI to Forgot password

--- frontend/src/paginas/OlvidePassword.jsx
+++ frontend/src/paginas/OlvidePassword.jsx
@@ -1,9 +1,54 @@
-import React from 'react'
+import { Link } from "react-router-dom";
 
 const OlvidePassword = () => {
   return (
-    <div>OlvidePassword</div>
-  )
-}
+    <>
+      <h1 className="text-sky-600 font-black text-6xl capitalize">
+        Recupera tu acceso y no pierdas tus{" "}
+        <span className="text-slate-700">proyectos</span>
+      </h1>
 
-export default OlvidePassword
+      <form className="my-10 bg-white shadow rounded-lg p-10">
+        <div className="my-5">
+          <label
+            htmlFor="email"
+            className="text-gray-600 uppercase block text-xl font-bold"
+          >
+            Email
+          </label>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
+            placeholder="Email de Registro"
+          />
+        </div>
+
+        <input
+          type="submit"
+          value="Enviar Instrucciones"
+          className="bg-sky-700 w-full mb-5 py-3 text-white uppercase font-bold rounded text-center hover:cursor-pointer hover:bg-sky-800 transition-colors"
+        />
+      </form>
+
+      <nav className="lg:flex lg:justify-between">
+        <Link
+          to="/"
+          className="block text-center my-5 text-slate-500 uppercase text-sm"
+        >
+          ¿Tienes una cuenta? Inicia sesión
+        </Link>
+
+        <Link
+          to="/registrar"
+          className="block text-center my-5 text-slate-500 uppercase text-sm"
+        >
+          ¿No tienes una cuenta? Regístrate
+        </Link>
+      </nav>
+    </>
+  );
+};
+
+export default OlvidePassword;
