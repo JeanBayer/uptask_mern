bcf18791a58ecfb21369ad9b8b9a3d1c5851de81
Jhan Bayer <jhanbayer@gmail.com>
2022-06-26 21:14:09 -0400

    feat: add UI to confirm account and new password

--- frontend/src/paginas/ConfirmarCuenta.jsx
+++ frontend/src/paginas/ConfirmarCuenta.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
 
 const ConfirmarCuenta = () => {
   return (
-    <div>ConfirmarCuenta</div>
-  )
-}
+    <>
+      <h1 className="text-sky-600 font-black text-6xl capitalize">
+        Confirma tu cuenta y empieza a crear tus{" "}
+        <span className="text-slate-700">proyectos</span>
+      </h1>
+    </>
+  );
+};
 
-export default ConfirmarCuenta
+export default ConfirmarCuenta;
--- frontend/src/paginas/NuevoPassword.jsx
+++ frontend/src/paginas/NuevoPassword.jsx
@@ -1,7 +1,36 @@
-import React from "react";
-
 const NuevoPassword = () => {
-  return <div>NuevoPassword</div>;
+  return (
+    <>
+      <h1 className="text-sky-600 font-black text-6xl capitalize">
+        Restablece tu password y no pierdas acceso a tus{" "}
+        <span className="text-slate-700">proyectos</span>
+      </h1>
+
+      <form className="my-10 bg-white shadow rounded-lg p-10">
+        <div className="my-5">
+          <label
+            htmlFor="password"
+            className="text-gray-600 uppercase block text-xl font-bold"
+          >
+            Nuevo Password
+          </label>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
+            placeholder="Escribe tu Nuevo Password"
+          />
+        </div>
+
+        <input
+          type="submit"
+          value="Guardar Nuevo Password"
+          className="bg-sky-700 w-full mb-5 py-3 text-white uppercase font-bold rounded text-center hover:cursor-pointer hover:bg-sky-800 transition-colors"
+        />
+      </form>
+    </>
+  );
 };
 
 export default NuevoPassword;
