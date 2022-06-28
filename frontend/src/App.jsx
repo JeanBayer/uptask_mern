53c8ccb6fff61f596368c73ad0dad3455804b3e9
Jhan Bayer <jhanbayer@gmail.com>
2022-06-28 16:59:17 -0400

    feat [frontend]: create Context Api

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,19 +5,22 @@ import Registrar from "./paginas/Registrar";
 import OlvidePassword from "./paginas/OlvidePassword";
 import NuevoPassword from "./paginas/NuevoPassword";
 import ConfirmarCuenta from "./paginas/ConfirmarCuenta";
+import { AuthProvider } from "./context/AuthProvider";
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<AuthLayout />}>
-          <Route index element={<Login />} />
-          <Route path="registrar" element={<Registrar />} />
-          <Route path="olvide-password" element={<OlvidePassword />} />
-          <Route path="olvide-password/:token" element={<NuevoPassword />} />
-          <Route path="confirmar/:id" element={<ConfirmarCuenta />} />
-        </Route>
-      </Routes>
+      <AuthProvider>
+        <Routes>
+          <Route path="/" element={<AuthLayout />}>
+            <Route index element={<Login />} />
+            <Route path="registrar" element={<Registrar />} />
+            <Route path="olvide-password" element={<OlvidePassword />} />
+            <Route path="olvide-password/:token" element={<NuevoPassword />} />
+            <Route path="confirmar/:id" element={<ConfirmarCuenta />} />
+          </Route>
+        </Routes>
+      </AuthProvider>
     </BrowserRouter>
   );
 }
---
+++ frontend/src/context/AuthProvider.jsx
@@ -0,0 +1,10 @@
+import { createContext, useState, useEffect } from "react";
+
+const AuthContext = createContext();
+
+const AuthProvider = ({ children }) => {
+  return <AuthContext.Provider value={{}}>{children}</AuthContext.Provider>;
+};
+
+export { AuthProvider };
+export default AuthContext;
