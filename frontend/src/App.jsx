715b92c81d5e4bbecd7126b2276b41f0ee93582d
Jhan Bayer <jhanbayer@gmail.com>
2022-06-29 11:09:43 -0400

    feat [frontend]: add new project page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,6 +8,7 @@ import ConfirmarCuenta from "./paginas/ConfirmarCuenta";
 import { AuthProvider } from "./context/AuthProvider";
 import RutaProtegida from "./layouts/RutaProtegida";
 import Proyectos from "./paginas/Proyectos";
+import NuevoProyecto from "./paginas/NuevoProyecto";
 
 function App() {
   return (
@@ -23,6 +24,7 @@ function App() {
           </Route>
           <Route path="/proyectos" element={<RutaProtegida />}>
             <Route index element={<Proyectos />} />
+            <Route path="crear-proyecto" element={<NuevoProyecto />} />
           </Route>
         </Routes>
       </AuthProvider>
---
+++ frontend/src/paginas/NuevoProyecto.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const NuevoProyecto = () => {
+  return (
+    <>
+      <h1 className="text-4xl font-black">Nuevo Proyecto</h1>
+      <div></div>
+    </>
+  );
+};
+
+export default NuevoProyecto;
