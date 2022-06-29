6cba6c13a7a6b9538c306da7e881f695fa73533f
Jhan Bayer <jhanbayer@gmail.com>
2022-06-29 10:26:02 -0400

    feat [frontend]: quey backend to see if the user is authenticated

--- frontend/src/context/AuthProvider.jsx
+++ frontend/src/context/AuthProvider.jsx
@@ -1,9 +1,49 @@
 import { createContext, useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import clienteAxios from "../config/clienteAxios";
 
 const AuthContext = createContext();
 
 const AuthProvider = ({ children }) => {
-  return <AuthContext.Provider value={{}}>{children}</AuthContext.Provider>;
+  const [auth, setAuth] = useState({});
+  const [cargando, setCargando] = useState(true);
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const autenticarUsuario = async () => {
+      const token = localStorage.getItem("token");
+      if (!token) {
+        setCargando(false);
+        return;
+      }
+
+      const config = {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      };
+
+      try {
+        const { data } = await clienteAxios.get("/usuarios/perfil", config);
+        setAuth(data);
+        navigate("/proyectos");
+      } catch (error) {
+        setAuth({});
+      } finally {
+        setCargando(false);
+      }
+    };
+
+    autenticarUsuario();
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ setAuth, auth, cargando }}>
+      {children}
+    </AuthContext.Provider>
+  );
 };
 
 export { AuthProvider };
