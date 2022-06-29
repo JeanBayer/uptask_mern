e0fce6628220d5808a7b39314cd1c5999a95c882
Jhan Bayer <jhanbayer@gmail.com>
2022-06-29 11:10:37 -0400

    feat [frontend]: add projects page

--- frontend/src/paginas/Proyectos.jsx
+++ frontend/src/paginas/Proyectos.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
 
 const Proyectos = () => {
   return (
-    <div>Proyectos</div>
-  )
-}
+    <>
+      <h1 className="text-4xl font-black">Proyectos</h1>
+      <div></div>
+    </>
+  );
+};
 
-export default Proyectos
+export default Proyectos;
