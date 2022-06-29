c08cb02458172d0afca7b9ecca711f0709cdf7cf
Jhan Bayer <jhanbayer@gmail.com>
2022-06-29 11:11:24 -0400

    feat [frontend]: add Sidebar and Header at Layout

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <header className="px-4 py-5 bg-white border-b">
+      <div className="md:flex md:justify-between md:items-center">
+        <h2 className="text-4xl text-sky-600 font-black text-center">UpTask</h2>
+        <input
+          type="search"
+          placeholder="Buscar Proyecto"
+          className="rounded-lg lg:w-96 block p-2 border"
+        />
+        <div className="flex items-center gap-4">
+          <Link to="/proyectos" className="font-bold uppercase">
+            Proyectos
+          </Link>
+          <button
+            type="button"
+            className="text-white text-sm bg-sky-600 p-3 rounded-md uppercase font-bold"
+          >
+            Cerrar Sesión
+          </button>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
---
+++ frontend/src/components/Sidebar.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import useAuth from "../hooks/useAuth";
+
+const Sidebar = () => {
+  const { auth } = useAuth();
+  return (
+    <aside className="md:w-72 lg:w-96 px-5 py-10">
+      <p className="text-xl font-bold">Hola: {auth.nombre}</p>
+      <Link
+        to="crear-proyecto"
+        className="bg-sky-600 w-full p3 text-white uppercase font-bold block text-center mt-5 rounded-lg"
+      >
+        Nuevo Proyecto
+      </Link>
+    </aside>
+  );
+};
+
+export default Sidebar;
